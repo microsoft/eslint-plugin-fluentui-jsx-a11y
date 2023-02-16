3870cf5c4919826fd584445bbf0fe1245cb8e41d
Roxana <roxpal@microsoft.com>
2023-02-16 14:37:43 +0000

    Fix comments

--- tests/lib/rules/image-link-missing-aria-v9.js
+++ tests/lib/rules/image-link-missing-aria-v9.js
@@ -29,10 +29,14 @@ const ruleTester = new RuleTester();
 ruleTester.run("image-link-missing-aria", rule, {
   valid: [
     '<Link href="#test"><Image src="/test.png"/>This is a label link</Link>',
+    '<Link href="#test"><Image src="/test.png"/><span>This is a label link</span></Link>',
+    '<Link href="#test"><Image src="/test.png"/><div><span>This is a label link</span></div></Link>',
     '<Link href="#test" title="This is a title for the link"><Image src="/test.png"/></Link>',
     '<Link href="#test"><Image src="/test.png" title="This is a title for the image link"/></Link>',
     '<Link href="#test"><Image src="/test.png" aria-label="This is an aria-label for the image link"/></Link>',
     '<Link href="#test"><Image src="/test.png" aria-labelledby="id1"/></Link>',
+    '<Link href="#test"><div><Image src="/test.png" aria-labelledby="id1"/></div></Link>',
+    '<Link href="#test"><div><Image src="/test.png"/>This is a label link</div></Link>',
   ],
 
   invalid: [
@@ -46,6 +50,10 @@ ruleTester.run("image-link-missing-aria", rule, {
     },
     {
       code: '<Link href="#test"><Image src="/test.png" title=""/></Link>',
+      errors: [{ messageId: "missingAriaLabel" }],
+    },
+    {
+      code: '<Link href="#test"><Image src="/test.png" aria-hidden="true" aria-label="This is aria label"/></Link>',
       errors: [{ messageId: "missingAriaLabel" }],
     },
   ],
