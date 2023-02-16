c29723bda248519519afb89824480738ab965720
Roxana <roxpal@microsoft.com>
2023-02-16 16:34:35 +0000

    Fix comments

--- tests/lib/rules/image-link-missing-aria-v9.js
+++ tests/lib/rules/image-link-missing-aria-v9.js
@@ -31,13 +31,15 @@ ruleTester.run("image-link-missing-aria", rule, {
     '<Link href="#test"><Image src="/test.png"/><span>This is a label link</span></Link>',
     '<Link href="#test"><Image src="/test.png"/><div><span>This is a label link</span></div></Link>',
     '<Link href="#test" title="This is a title for the link"><Image src="/test.png"/></Link>',
+    '<Link href="#test" aria-label="This is a aria-label for the link"><Image src="/test.png"/></Link>',
+    '<Link href="#test" aria-labelledby="This is a aria-labelledby for the link"><Image src="/test.png"/></Link>',
     '<Link href="#test"><Image src="/test.png" title="This is a title for the image link"/></Link>',
+    '<Link href="#test"><div><Image src="/test.png" title="This is a title for the image link"/></div></Link>',
     '<Link href="#test"><Image src="/test.png" aria-label="This is an aria-label for the image link"/></Link>',
     '<Link href="#test"><Image src="/test.png" aria-labelledby="id1"/></Link>',
     '<Link href="#test"><div><Image src="/test.png" aria-labelledby="id1"/></div></Link>',
     '<Link href="#test"><div><Image src="/test.png"/>This is a label link</div></Link>',
   ],
-
   invalid: [
     {
       code: '<Link href="#test"><Image src="/test.png"/></Link>',
