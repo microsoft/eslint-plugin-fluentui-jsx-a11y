5e9f0fe35b62317c458d9d63f1d5a22d91fee686
nikitasingla <nikitasingla@I2202.guest.corp.microsoft.com>
2024-03-13 10:11:22 +0530

    adding labelling lint rules for toggle button, the case with button and icons was already handled

---
+++ docs/rules/toggle-button-needs-labelling-v9.md
@@ -0,0 +1,40 @@
+# Accessibility: Toggle buttons must have accessible labelling: title, aria-label, aria-labelledby, aria-describedby (`@microsoft/fluentui-jsx-a11y/toggle-button-needs-labelling-v9`)
+
+<!-- end auto-generated rule header -->
+
+All interactive elements must have an accessible name.
+
+Image buttons without additional text content lack an accessible name.
+
+Please add title, aria-label, aria-labelledby, aria-described by etc.
+
+<https://www.w3.org/TR/html-aria/>
+
+## Rule Details
+
+This rule aims to prevent an icon button from not having an accessible name.
+
+Examples of **incorrect** code for this rule:
+
+```jsx
+<ToggleButton/>
+<ToggleButton></ToggleButton>
+
+<Label id="calendar-2">Start date</Label>
+<ToggleButton aria-labelledby="calendar-" />
+```
+
+Examples of **correct** code for this rule:
+
+```jsx
+<ToggleButton title="Toggle example" />
+<ToggleButton aria-label="Toggle example" />
+<ToggleButton>Toggle example</ToggleButton>
+
+<Label id="calendar-1">Toggle example</Label>
+<ToggleButton aria-labelledby="calendar-1" />
+
+<Tooltip content="Toggle example" relationship="label">
+    <ToggleButton icon={<CalendarMonthRegular />} />
+</Tooltip>
+```
---
+++ tests/lib/rules/toggle-button-needs-labelling-v9.js
@@ -0,0 +1,53 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/toggle-button-needs-labelling-v9"),
+    RuleTester = require("eslint").RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester();
+ruleTester.run("toggle-button-needs-labelling-v9", rule, {
+    valid: [
+        '<ToggleButton aria-label="Toggle Example" />',
+        '<ToggleButton aria-label="Toggle Example"></ToggleButton>',
+        "<ToggleButton>Example</ToggleButton>",
+        '<ToggleButton title="Toggle Example"></ToggleButton>',
+        '<Tooltip content="Toggle Example" relationship="label"><ToggleButton />} /></Tooltip>',
+        '<><Label id="label-id-4">Toggle Example</Label><ToggleButton aria-labelledby="label-id-4"></ToggleButton></>',
+        '<><Label id="label-id-4">Toggle Example</Label><ToggleButton aria-labelledby="label-id-4" /></>',
+        '<><label id="label-id-4">Toggle Example</label><ToggleButton aria-labelledby="label-id-4"></ToggleButton></>',
+        '<><label id="label-id-4">Toggle Example</label><ToggleButton aria-labelledby="label-id-4" /></>'
+    ],
+    invalid: [
+        {
+            code: "<ToggleButton></ToggleButton>",
+            errors: [{ messageId: "missingAriaLabel" }]
+        },
+        {
+            code: "<ToggleButton />",
+            errors: [{ messageId: "missingAriaLabel" }]
+        },
+        {
+            code: '<ToggleButton aria-labelledby="label-id-4"></ToggleButton>',
+            errors: [{ messageId: "missingAriaLabel" }]
+        },
+        {
+            code: '<><Label id="label-id-4">Toggle Example</Label><ToggleButton aria-labelledby="label-id-5" /></>',
+            errors: [{ messageId: "missingAriaLabel" }]
+        },
+        {
+            code: "<><Label>Toggle Example</Label><ToggleButton/></>",
+            errors: [{ messageId: "missingAriaLabel" }]
+        }
+    ]
+});
+
