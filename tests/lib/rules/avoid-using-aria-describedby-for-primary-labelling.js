7385185d21b6d11b94e8d728625f534bc6c9f9a8
Aubrey Quinn <aubreyquinn@microsoft.com>
2024-09-17 17:10:41 +0100

    updated test file

--- tests/lib/rules/avoid-using-aria-describedby-for-primary-labelling.js
+++ tests/lib/rules/avoid-using-aria-describedby-for-primary-labelling.js
@@ -26,27 +26,22 @@ RuleTester.setDefaultConfig({
 const ruleTester = new RuleTester();
 ruleTester.run("avoid-using-aria-describedby-for-primary-labelling", rule, {
     valid: [
-        '<><Button aria-label="submit" aria-describedby="submitDesc" icon={<CalendarMonthRegular />} /><p id="submitDesc">Click to submit your form. This will save your data.</p></>',
-        '<><Button aria-describedby="submitDesc">Submit</Button><p id="submitDesc">Click to submit your form. This will save your data.</p></>',
-        '<><Label htmlFor="myInput">Name</Label><TextField id="myInput" aria-describedby="nameDesc" placeholder="Enter your name" /><p id="nameDesc">This field is for your full legal name.</p></>'
+        '<><Button aria-label="submit" aria-describedby="submitDesc" icon={<CalendarMonthRegular />} /><Label id="submitDesc">Click to submit your form. This will save your data.</Label></>',
+        '<><Button aria-describedby="submitDesc">Submit</Button><Label id="submitDesc">Click to submit your form. This will save your data.</Label></>',
+        '<><Label htmlFor="myInput">Name</Label><TextField id="myInput" aria-describedby="nameDesc" placeholder="Enter your name" /><Label id="nameDesc">This field is for your full legal name.</Label></>'
     ],
     invalid: [
         {
-            code: '<><Button aria-describedby="submitDesc" icon={<CalendarMonthRegular />} /><p id="submitDesc">Click to submit your form. This will save your data.</p></>',
+            code: '<><Button aria-describedby="submitDesc" icon={<CalendarMonthRegular />} /><Label id="submitDesc">Click to submit your form. This will save your data.</Label></>',
             errors: [{ messageId: "noAriaDescribedbyAsLabel" }]
         },
         {
-            code: '<><TextField id="myInput" aria-describedby="nameDesc" /><p id="nameDesc">Name</p></>',
+            code: '<><TextField id="myInput" aria-describedby="nameDesc" /><Label id="nameDesc">Name</Label></>',
             errors: [{ messageId: "noAriaDescribedbyAsLabel" }]
         },
         {
             code: '<><Input type="text" aria-describedby="nameDesc" /><Label id="nameDesc">Name</Label></>',
             errors: [{ messageId: "noAriaDescribedbyAsLabel" }]
-        },
-        {
-            code: '<><Input type="text" aria-describedby="nameDesc" /><p id="nameDesc">Name</p></>',
-            errors: [{ messageId: "noAriaDescribedbyAsLabel" }]
         }
     ]
 });
-
