a4c3fd45bc697bacb064f93de219f7d9b463285b
Aubrey Quinn <aubreyquinn@microsoft.com>
2024-09-27 22:05:48 +0100

    added code coverage threasholds

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,14 @@ module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
     testMatch: ["**/tests/**/*.test.ts"],
-    verbose: true
+    verbose: true,
+    collectCoverage: true,
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80
+        }
+    }
 };
