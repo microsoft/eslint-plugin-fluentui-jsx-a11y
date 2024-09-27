b79aaa7f8e1c273f87052caae5e11f4d40d0a1c3
Aubrey Quinn <aubreyquinn@microsoft.com>
2024-09-27 17:43:53 +0100

    delete dist files

--- dist/lib/applicableComponents/ariaLabelBasedComponent.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-const { applicableComponents: ButtonBasedComponents } = require("./buttonBasedComponents");
-const { applicableComponents: InputBasedComponents } = require("./inputBasedComponents");
-const applicableComponents = ["SpinButton", ...ButtonBasedComponents, ...InputBasedComponents];
-module.exports = {
-    applicableComponents
-};
